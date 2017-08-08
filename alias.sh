340956d253a84fe60c9cf5850bc636901772b283
lilongen <lilongen@gmail.com>
2017-08-08 17:07:02 +0800

    1

--- alias.sh
+++ alias.sh
@@ -5,4 +5,6 @@ D="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source $D/vars.sh
 
 alias resocksec2="pkill -9 -f '$awsHost';ssh -g -f -N -D 0.0.0.0:12306 -i $awsHostKey  ec2-user@$awsHost 2&>/dev/null;pgrep -l -f '$awsHost'"
-alias tsocks="curl --proxy socks5h://localhost:12306 http://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png -I -w %{http_code} -s -o /dev/null"
+
+resource="http://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+alias tsocks="curl --proxy socks5h://localhost:12306 $resource -I -w %{http_code} -s -o /dev/null"
