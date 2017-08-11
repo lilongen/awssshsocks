75d2974a301ba3c1df4de12876453841d607d0da
lilongen <lilongen@gmail.com>
2017-08-11 14:45:21 +0800

    1

--- alias.sh
+++ alias.sh
@@ -1,9 +1,6 @@
 #/bin/bash
 #
 
-D="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-source $D/vars.sh
-
 alias resocksec2="pkill -9 -f '$awsHost';ssh -g -f -N -D 0.0.0.0:12306 -i $awsHostKey  ec2-user@$awsHost 2&>/dev/null;pgrep -l -f '$awsHost'"
 
 resource="http://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
